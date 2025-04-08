5c315754b0c645119bfcdbbedc134c6e81b2a0f1
brianlck <brianlee.lck@gmail.com>
2025-04-08 17:51:55 -0400

    a bad sampling algorithm

--- sampling.py
+++ sampling.py
@@ -1,7 +1,10 @@
+
+# TOOD: This is a very inflexible sampling algorithm -- Only works for semiautoregressive with one token addition at one time
+# TODO: This code is quite bad, we'd like to refactor, can we use ein / einops?
 import torch    
 
 
-def euler_sampling(model: torch.nn.Module, steps: int, mask: int, pad: int, batch_size: int, max_length: int):
+def semiauto_euler_sampling(model: torch.nn.Module, steps: int, mask: int, pad: int, batch_size: int, max_length: int):
     device = model.device
     xt = pad * torch.ones_like((batch_size, max_length), device=device)
 
@@ -9,6 +12,30 @@ def euler_sampling(model: torch.nn.Module, steps: int, mask: int, pad: int, batc
     for i in range(steps):
         t = 1.0 - dt * i 
         unmask_rate, len_rate = model(xt, t)
+
+        # Probabilistically unmask token
+        mask_positions = (xt == mask)
+        other_rates_sum = torch.sum(unmask_rate[mask_positions, :mask], dim=1) + torch.sum(unmask_rate[mask_positions, mask+1:], dim=1)
+        unmask_rate[mask_positions, mask] = -other_rates_sum
+        
+        # Zero out rates for non-mask positions
+        non_mask_positions = ~mask_positions
+        unmask_rate[non_mask_positions] = 0
+
+        unmask_rate = unmask_rate * dt
+
+        batch_indices = torch.arange(batch_size, device=device)
+        pos_indices = torch.arange(max_length, device=device)
+        xt_indices = xt[batch_indices.unsqueeze(1), pos_indices]
+        unmask_rate[batch_indices.unsqueeze(1), pos_indices, xt_indices] += 1
+
+        # Sample from categorical distribution for each position
+        # Get probabilities for all mask positions at once
+        mask_probs = torch.softmax(unmask_rate[mask_positions], dim=-1)
+        # Sample from categorical distribution for all mask positions at once
+        sampled_tokens = torch.multinomial(mask_probs, 1).squeeze(-1)
+        # Update xt with sampled tokens at mask positions
+        xt[mask_positions] = sampled_tokens
         
         extension_prob = len_rate * dt 
         extension = torch.bernoulli(extension_prob)
@@ -22,6 +49,10 @@ def euler_sampling(model: torch.nn.Module, steps: int, mask: int, pad: int, batc
         pos_indices = first_pad_pos[valid_mask & extension]
         # Set mask token at first pad position for valid sequences that extend
         xt[batch_indices, pos_indices] = mask
+
+
+    
+
 
         
 
